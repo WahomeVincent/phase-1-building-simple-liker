99af658aa139bc34f3dd834d45a2a66586308c08
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-04-01 10:04:28 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -5,8 +5,52 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 // Hide an element with the class "hidden-element"
-(".modal").hide();
+/* (".modal").hide(); */
 
+/* document.addEventListener('DOMContentLoaded', () => {
+  const errorModal = document.querySelector('#modal');
+  errorModal.classList.add('hidden');
+}); */
+
+const errorModal = document.querySelector('#modal');
+const emptyHearts = document.querySelectorAll('.like-glyph');
+const errorMessage = document.querySelector('#modal-message');
+
+errorModal.classList.add('hidden');
+
+function handleServerResponse(response) {
+  if (response === 'success') {
+    return Promise.resolve();
+  } else {
+    return Promise.reject();
+  }
+}
+
+function toggleHeart(event) {
+  const heart = event.target;
+  mimicServerCall()
+    .then(response => handleServerResponse(response))
+    .then(() => {
+      if (heart.classList.contains('activated-heart')) {
+        heart.classList.remove('activated-heart');
+      } else {
+        heart.classList.add('activated-heart');
+      }
+      heart.classList.toggle('like-glyph');
+      heart.classList.toggle('liked-glyph');
+    })
+    .catch(error => {
+      errorMessage.textContent = error;
+      errorModal.classList.remove('hidden');
+      setTimeout(() => {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+}
+
+for (let heart of emptyHearts) {
+  heart.addEventListener('click', toggleHeart);
+}
 
 
 
